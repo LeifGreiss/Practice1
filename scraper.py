7d45bdfd8e3cdeae03d1f1730266e57ecf1a2b33
Leif Greiss <helter1234@gmail.com>
2017-12-05 16:25:08 -0500

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -3,7 +3,7 @@ import lxml.html
 # scrape_table function: gets passed an individual page to scrape
 # where is root coming from?
 def scrape_table(root):
-    rows = root.cssselect("p.ex1")
+    rows = root.cssselect("paragraph.ex1")
     #rows = root.cssselect("table.Trolley.table tr")  # selects all <tr> blocks within <table class="Trolley">
     for row in rows: # where do rows come from? 
         # Set up our data record 
