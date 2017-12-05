8090b32d4c0c7d2f2544a0db18468cd9c0f564da
Leif Greiss <helter1234@gmail.com>
2017-12-05 16:32:00 -0500

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -3,15 +3,15 @@ import lxml.html
 # scrape_table function: gets passed an individual page to scrape
 # where is root coming from?
 def scrape_table(root):
-    rows = root.cssselect("paragraph.ex1")
+    rows = root.cssselect("paragraph.ex1 TR")
     #rows = root.cssselect("table.Trolley.table tr")  # selects all <tr> blocks within <table class="Trolley">
     for row in rows: # where do rows come from? 
         # Set up our data record 
         record = {}
         table_cells = row.cssselect("TD") #In the row use cssselect to select for p
         if table_cells: 
-            record['Racecourse'] = table_cells[0].text_content
-            record['Address and Phone Book'] = table_cells[1].text_content
+            record['Racecourse'] = table_cells[0].text
+            record['Address and Phone Book'] = table_cells[1].text
             # Print out the data we've gathered
             print record, '------------'
             # Save the record to the data store with Hospital as the unique key.
